b9fa3b936fad4c3bafccac15073bda9a929518e2
Claire Peng <claire.peng@fortify.pro>
2019-04-15 11:40:43 -0700

    Added build folder

---
+++ build/precache-manifest.29f0ffc4e6cd4d950c6f053f45cd647d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "80f757ab402ac45d55ff",
+    "url": "/static/js/main.e4fcfd13.chunk.js"
+  },
+  {
+    "revision": "f42729402dce21f11929",
+    "url": "/static/js/2.7daa6139.chunk.js"
+  },
+  {
+    "revision": "80f757ab402ac45d55ff",
+    "url": "/static/css/main.a42e192b.chunk.css"
+  },
+  {
+    "revision": "aec2fdc514b3fae18d6cbf0805063dca",
+    "url": "/index.html"
+  }
+];
